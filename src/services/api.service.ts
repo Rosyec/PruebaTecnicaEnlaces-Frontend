3599c421c260f9da7a6320e9aa71639bc4642588
Rosyec <chechomens@gmail.com>
2023-04-14 22:44:32 -0500

    Vercel.json

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,7 +1,7 @@
 import type { MusicBrainzDta } from '@/helpers/data.interface';
 import axios from 'axios';
 
-const API = axios.create({baseURL: 'http://localhost:3005'});
+const API = axios.create({baseURL: 'https://prueba-tecnica-enlaces-backend.vercel.app'});
 
 async function getAllGenres( limit: number, offset: number ) {
     const response = await API.get<MusicBrainzDta>(`getAllGenresByCount?limit=${limit}&offset=${offset}`);
